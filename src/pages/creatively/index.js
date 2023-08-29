2edb9c487312427175b34707890433aed70d5a94
Iraguha Lionel <liraguha@qtsoftwareltd.com>
2023-08-29 09:45:54 +0200

    New changes

--- src/pages/creatively/index.js
+++ src/pages/creatively/index.js
@@ -10,7 +10,7 @@ import PortfolioPage from "@/components/PortfolioPage";
 
 const videos = [
   {
-    title: 'The ‘Kivugo’ Commercial, 2023',
+    title: 'The ‘Icyivugo’ Commercial, 2023',
     category: 'Conceptualizing, script writing, casting, sound production, video production and editing, animation',
     thumbnailUrl: 'https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=80',
     videoUrl: "https://res.cloudinary.com/dyn4t1qng/video/upload/v1692625893/Ndi_Rwema_TVC_cnftxc.mp4",
