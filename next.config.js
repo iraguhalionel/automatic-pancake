b5bc86db105c7761e1c649a634f9ffa470df6e79
Iraguha Lionel <liraguha@qtsoftwareltd.com>
2024-01-07 12:49:15 +0200

    Static build

--- next.config.js
+++ next.config.js
@@ -1,21 +1,9 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
   images: {
     domains: ["res.cloudinary.com"],
   },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(mp4|webm)$/, // Adjust the file extensions as needed
-      use: {
-        loader: "file-loader",
-        options: {
-          publicPath: "/_next/static/files", // Adjust the public path as needed
-          outputPath: "static/files", // Adjust the output path as needed
-          name: "[name].[ext]",
-          esModule: false,
-        },
-      },
-    });
+}
 
-    return config;
-  },
-};
+module.exports = nextConfig
